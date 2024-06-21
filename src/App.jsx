7aafcd7e4ce2c6ed65840a70859043224bf01e2d
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-21 16:17:11 +0530

    Employe Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-const sub=["PHP","ORACLE","JAVA","PYTHON"];
-
-  const mysub=sub.map((key)=>{
-    return(
-      <>
-      <li>{key}</li>
-      </>
-    )
-  })
-
+import Employee from "./Employee";
+const myans=Employee.map((key)=>{
+  return(
+    <>
+    <tr>
+      <td>{key.empno}</td>
+      <td>{key.name}</td>
+      <td>{key.salary}</td>
+      <td>{key.city}</td>
+    </tr>
+    </>
+  )
+})
 const App=()=>{
   return(
     <>
-     <h1>Welcome to cybrom </h1>
-     <ol>
-      {mysub}
-     </ol>
+     <h1 align="center">Welcome to cybrom </h1>
+    <table align="center" border="1" width="500" bgcolor="lightblue">
+      <tr bgcolor="grey">
+      <th>Employee No</th>
+      <th>Name</th>
+      <th>Salary</th>
+      <th>City</th>
+      </tr>
+      {myans}
+    </table>
     </>
   )
 }
