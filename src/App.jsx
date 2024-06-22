07d0dfda041f7ff67cd625b310f37b143545acdf
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-22 18:44:33 +0530

    CSS In React

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import Employee from "./Employee";
-const myans=Employee.map((key)=>{
-  return(
-    <>
-    <tr>
-      <td>{key.empno}</td>
-      <td>{key.name}</td>
-      <td>{key.salary}</td>
-      <td>{key.city}</td>
-    </tr>
-    </>
-  )
-})
 const App=()=>{
   return(
+    // <>
+    // <div style={{backgroundColor:"pink",border:"1", borderRadius:"10", width:"300px",hight:"300px"}}>
+    //   <div style={{backgroundColor:"red",border:"1", borderRadius:"10", width:"100px",hight:"100px",position:"relative",top:"20px",left:"20px"}}></div>
+    // </div>
+    // </>
     <>
-     <h1 align="center">Welcome to cybrom </h1>
-    <table align="center" border="1" width="500" bgcolor="lightblue">
-      <tr bgcolor="grey">
-      <th>Employee No</th>
-      <th>Name</th>
-      <th>Salary</th>
-      <th>City</th>
-      </tr>
-      {myans}
-    </table>
+    <div style={{backgroundColor:"red", width:"400px",height:"400px",borderRadius:"20px"}}>
+      <div style={{backgroundColor:"grey",width:"300px", height:"300px",borderRadius:"20px",position:"relative", left:"45px",top:"45px"}}>
+        <div style={{backgroundColor:"yellow",width:"200px",height:"200px",borderRadius:"20px",position:"relative",left:"50px",top:"50px"}}>
+             <div style={{backgroundColor:"blue",width:"150px",height:"150px",borderRadius:"20px",position:"relative",left:"25px",top:"25px"}}></div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
