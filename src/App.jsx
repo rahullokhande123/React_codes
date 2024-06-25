6bb24ae4bd6839d0f9064675959c93d80f562802
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-25 15:17:55 +0530

    BootStrap_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
+//----------------------------------------------
 const App=()=>{
   return(
     <>
-      <div id="box1">
-        <div id="box2">
-          <div id="box3">
-            <div id="box4">
-              <div id="box5">Cybrom</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+      
+      <Container>
+        <Alert variant='success' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+        <Alert variant='danger' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+        <Alert variant='dark' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+        <Alert variant='info' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+        <Alert variant='primary' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+        <Alert variant='success' dismissible>
+          This is a alert—check it out!
+        </Alert>
+
+      </Container>
+       
+   </>
   )
 }
 export default App;
