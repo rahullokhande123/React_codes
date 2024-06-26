9d82bed05a092d7ff8934a06c83f7a4aea72401b
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-26 14:47:58 +0530

    Bootstrap_layout3

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ const App=()=>{
  return(
   <>
     <Container fluid className="text-center border p-5">  
-      {/* fluid -> ka use hm container ke sath krte h bada container lene ke */}
-
-{/* Container me bhi hm 12 Section bna sakte h md {} ki help se 
-(count krne pr eska total 12 hi aana chahiye) */}
-
+      {/* fluid -> ka use hm container ke sath krte h bada container lene ke liye */}
       <Row>
-        <Col md={3} className="border bg-info p-5 text-white">
+        <Col className="border bg-info p-5 text-white">
         <h1>Wellcome to Cybrom!!!</h1>
         </Col>
-        <Col md={3} className="border bg-success p-5 text-white fs-6 ">
+        <Col className="border bg-success p-5 text-white fs-6 ">
         <h1>Hello to Cybrom!!!</h1>
         </Col>
-        <Col md={6} className="border bg-primary p-5 text-white">
+        <Col className="border bg-primary p-5 text-white">
             <h1>I am Devloper</h1>
         </Col>
       </Row>
