fa0bc9493ccce8b26c1775ecc739c096d2e86a95
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-26 14:46:02 +0530

    Bootstrap_layout2

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,20 @@ import { Container,Row, Col } from "react-bootstrap";
 const App=()=>{
  return(
   <>
-    <Container fluid className="text-center border p-5 bg-dark">  
+    <Container fluid className="text-center border p-5">  
       {/* fluid -> ka use hm container ke sath krte h bada container lene ke */}
 
 {/* Container me bhi hm 12 Section bna sakte h md {} ki help se 
 (count krne pr eska total 12 hi aana chahiye) */}
 
       <Row>
-        <Col md={6} className="border bg-info p-5 text-white mb-5">
+        <Col md={3} className="border bg-info p-5 text-white">
         <h1>Wellcome to Cybrom!!!</h1>
         </Col>
-        <Col md={3} className="border bg-success p-5 text-white fs-6 mb-5">
+        <Col md={3} className="border bg-success p-5 text-white fs-6 ">
         <h1>Hello to Cybrom!!!</h1>
         </Col>
-        <Col md={3} className="border bg-primary p-5 text-white mb-5 ">
-            <h1>I am Devloper</h1>
-        </Col>
-
-
-        <Col md={8} className="border bg-info p-5 text-white mb-5">
-        <h1>Wellcome to Cybrom!!!</h1>
-        </Col>
-        <Col md={2} className="border bg-success p-5 text-white fs-6 mb-5">
-        <h1>Hello to Cybrom!!!</h1>
-        </Col>
-        <Col md={2} className="border bg-primary p-5 text-white mb-5">
+        <Col md={6} className="border bg-primary p-5 text-white">
             <h1>I am Devloper</h1>
         </Col>
       </Row>
