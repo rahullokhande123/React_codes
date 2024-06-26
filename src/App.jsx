487f80fd9bc8e0849005a087215429bba8dbf684
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-26 14:56:55 +0530

    Bootstrap_layout4

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import { Container,Row, Col } from "react-bootstrap";
 const App=()=>{
  return(
   <>
-    <Container fluid className="text-center border p-5">  
-      {/* fluid -> ka use hm container ke sath krte h bada container lene ke liye */}
+    <Container className="text-center border p-5 m-5">
       <Row>
-        <Col className="border bg-info p-5 text-white">
+        <Col className="border bg-danger p-5 text-white">
         <h1>Wellcome to Cybrom!!!</h1>
         </Col>
         <Col className="border bg-success p-5 text-white fs-6 ">
         <h1>Hello to Cybrom!!!</h1>
-        </Col>
-        <Col className="border bg-primary p-5 text-white">
-            <h1>I am Devloper</h1>
         </Col>
       </Row>
     </Container>
