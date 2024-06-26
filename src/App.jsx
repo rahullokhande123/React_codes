de0e8c14217590151be11d30ec2fe537cff6671f
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-26 14:59:11 +0530

    Bootstrap_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { Container,Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 const App=()=>{
  return(
   <>
-    <Container className="text-center border p-5 m-5">
-      <Row>
-        <Col className="border bg-danger p-5 text-white">
-        <h1>Wellcome to Cybrom!!!</h1>
-        </Col>
-        <Col className="border bg-success p-5 text-white fs-6 ">
-        <h1>Hello to Cybrom!!!</h1>
-        </Col>
-      </Row>
+    <Container className="text-center border p-5 m-5 bg-info">
+      <h1>Wellcome to Cybrom!!!</h1>
     </Container>
   </>
  )
