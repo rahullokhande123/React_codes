1b4c033b75172199da905629f3c495e246488fdf
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-03 15:39:55 +0530

    Counter Program

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 const App=()=>{
-  const display=(nm, c)=>{
-         alert(nm+ " Event Type : " + c.type );
-  }
-
+    const [cnt,setCnt]=useState(0);
+    const myInc=()=>{
+      setCnt(cnt+1)
+    }
+    const myDec=()=>{
+      setCnt(cnt-1)
+    }
+    const myRes=()=>{
+      setCnt(0)
+    }
   return(
     <>
-      <h1>Wellcome To Gaming World</h1>
-      <button onClick={(e)=>{display("Rahul ", e)}} >Click Here</button>
+      <center>
+      <h1>Please Count Actions</h1>
+      <h1> Count : {cnt}</h1>
+      <button onClick={myInc}> Increment </button>
+      <button onClick={myDec}> Decriment </button>
+      <button onClick={myRes}> Reset</button>
+      </center>
+      
     </>
   )
 }
