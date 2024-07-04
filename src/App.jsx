b104310756dceda1a794a2918e1aa21595399fee
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-04 15:43:10 +0530

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import { useState } from "react"
-
+import { useState } from "react";
 const App=()=>{
-  const [empno,setEmpno]=useState("");
-  const [name,setName]=useState("");
-  const [city,setCity]=useState("");
-  const [salary,setSalary]=useState("");
-
-  const submitHandle=()=>{
-    let store={empno:empno,name:name,city:city,salary:salary};
-    console.log(store);
-  }
-
-     
+     const [name,setName] = useState("");
+     const [city,setCity] =useState("");
+     const submitHandle =()=>{
+      let inputVal={name:name,city:city}
+      console.log(inputVal);
+     }
   return(
     <>
       <h1> Application Form </h1>
-      Enter Empno: <input type="text" value={empno} onChange={(e)=>{setEmpno(e.target.value)}} />
+      Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
       <br />
-      Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}  />
-      <br />
-      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}  />
-      <br />
-      Enter Salary <input type="text" value={salary} onChange={(e)=>{setSalary(e.target.value)}}  />
+      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}} />
       <br />
       <button onClick={submitHandle}>Data Save</button>
 
