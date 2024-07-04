6ca5c0216b8c2d2d742d13863e0ced985e9cada0
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-04 15:41:17 +0530

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useState } from "react"
+
 const App=()=>{
-     const [name,setName] = useState("Rahul")
+  const [empno,setEmpno]=useState("");
+  const [name,setName]=useState("");
+  const [city,setCity]=useState("");
+  const [salary,setSalary]=useState("");
+
+  const submitHandle=()=>{
+    let store={empno:empno,name:name,city:city,salary:salary};
+    console.log(store);
+  }
+
+     
   return(
     <>
-      <h1>Hello we are Cybrom Student : {name}</h1>
-      <button onClick={()=>{setName("ARUN")}} >Click Here</button>
+      <h1> Application Form </h1>
+      Enter Empno: <input type="text" value={empno} onChange={(e)=>{setEmpno(e.target.value)}} />
+      <br />
+      Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}  />
+      <br />
+      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}  />
+      <br />
+      Enter Salary <input type="text" value={salary} onChange={(e)=>{setSalary(e.target.value)}}  />
+      <br />
+      <button onClick={submitHandle}>Data Save</button>
+
     </>
   )
 }
