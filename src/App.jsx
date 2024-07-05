3b7d004a04ab8f802c7357e070ed00cc2028e62f
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-05 15:16:26 +0530

    Hook_useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ const App=()=>{
   const [cnt,setCnt]=useState(0);
 
   useEffect(()=>{
-    
+    setTimeout(()=>{
       setCnt(cnt+1);
-    
-  },[]);
+    },2000);
+  });
    return(
     <>
       <h1>Application Form :{cnt} </h1>
