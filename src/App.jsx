9d78f42582f23b2158a7fb33ab5cea1131532a74
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-05 15:14:19 +0530

    Hook_useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-
 const App=()=>{
 
   const [cnt,setCnt]=useState(0);
-  const [multi,setMulti]=useState(1);
 
-    useEffect(()=>{
-      setMulti(cnt*2);
-    },[cnt])
-
+  useEffect(()=>{
+    
+      setCnt(cnt+1);
+    
+  },[]);
    return(
     <>
-       <button onClick={()=>{setCnt(cnt+1)}} >Click Here</button>
       <h1>Application Form :{cnt} </h1>
-      <h1>Multiplication:{multi}</h1>
     </>
    )
 }
