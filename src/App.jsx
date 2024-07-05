0226dd1f6f54d169cb993c37ba718ea167849c7f
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-05 15:10:31 +0530

    Hook_useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
-const App=()=>{
-     const [name,setName] = useState("");
-     const [city,setCity] =useState("");
-     const submitHandle =()=>{
-      let inputVal={name:name,city:city}
-      console.log(inputVal);
-     }
-  return(
-    <>
-      <h1> Application Form </h1>
-      Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} />
-      <br />
-      Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}} />
-      <br />
-      <button onClick={submitHandle}>Data Save</button>
+import { useEffect } from "react";
 
+
+const App=()=>{
+
+  const [cnt,setCnt]=useState(0);
+  const [multi,setMulti]=useState(1);
+
+    useEffect(()=>{
+      setMulti(cnt*2);
+    },[cnt])
+
+   return(
+    <>
+       <button onClick={()=>{setCnt(cnt+1)}} >Click Here</button>
+      <h1>Application Form :{cnt} </h1>
+      <h1>Multiplication:{multi}</h1>
     </>
-  )
+   )
 }
 export default App;
