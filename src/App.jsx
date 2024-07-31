aa4a46cc3af07d31316179384f6fddfb0b22f400
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-31 22:53:55 +0530

    To Do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,44 @@
-// import { useSelector,useDispatch } from "react-redux";
-// import { myinc,mydic } from "./counterSlice";
-
-// const App=()=>{
-
-//     const Counter=useSelector((state)=>state.mycounter.cnt)
-//     const mydis=useDispatch();
-//     return(
-//         <>
-//         <h1>My Counter</h1>
-//         <button onClick={()=>{mydis(myinc())}}>Increment</button>
-//         <h1> {Counter} </h1>
-//         <button onClick={()=>{mydis(mydic())}}>Decrement</button>
-//         </>
-//     )
-// }
-// export default App;
-
-// useSelecter ka use 
-
-// =====================================================
-
-// import { useSelector,useDispatch } from "react-redux";
-// import { changeColor } from "./colorSlice";
-// import { useState } from "react";
-
-// const App=()=>{
-//     const myclr=useSelector((state)=>state.mycolor.bgcolor);
-//     const mydis=useDispatch();
-//     const [clr,setClr]=useState("")
-
-//     return(
-//         <>
-//         <h1>Redux Tool Kit </h1>
-//         Enter Color Name: <input type="text" value={clr} onChange={(e)=>{setClr(e.target.value)}} />
-//         <button onClick={()=>{mydis(changeColor(clr))}}>Change</button>
-//         <div style={{width:"400px",height:"400px",backgroundColor:myclr,border:"1px solid black",borderRadius:"10%",margin:"50px"}}></div>
-//         </>
-//     )
-// }
-// export default App;
-
-import { useSelector,useDispatch } from "react-redux";
-import { changeColor } from "./colorSlice";
 import { useState } from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { addTask } from "./toDoSlice";
+
 
 const App=()=>{
-
-    const myclr=useSelector((state)=>state.mycolor.bgcolor);
+    const [val,setVal]=useState("");
+    const mytask=useSelector((state)=>state.todolist.task);
     const mydis=useDispatch();
-    const [input,setInput]=useState("");
+
+    const taskAdd=()=>{
+       mydis(addTask({id: Date.now(), task:val}))
+       setVal("");
+    }
+    let sno=0
+    
+
+    const ans = mytask.map((key)=>{
+        sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>{key.task}</td>
+            </tr>
+            </>
+        )
+    })
     return(
         <>
-        Enter Color Name <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}} />
-        <button onClick={()=>{mydis(changeColor(input))}}>Change</button>
-        <div style={{width:"400px",height:"400px",backgroundColor:myclr,border:"1px solid black",borderRadius:"10%",margin:"50px"}}></div>
+        <h1>To Do App </h1>
+        Enter <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} />
+        <button onClick={taskAdd}>Add</button>
+        <hr size="4" color="blue" />
+        
+            <tr>
+                <th>S no.</th> 
+                <th>Task</th>
+            </tr>
+            {ans}
+        
         </>
     )
 }
