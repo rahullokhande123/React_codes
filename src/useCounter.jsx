b5fc58d48b28d5414580a0cf28b5e7021eedb92a
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-25 10:56:27 +0530

    React.js

--- src/useCounter.jsx
+++ src/useCounter.jsx
@@ -4,10 +4,12 @@ const useCounter=()=>{
 
     const [count2,setCnt]=useState(0)
 
-    
+    const Increment=()=>{
+        setCnt(count2 + 1)
+    }
 
     
 
-    
+    return [count2,Increment,Decrement]
 }
 export default useCounter;
